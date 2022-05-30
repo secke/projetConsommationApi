b325d12c64c2cda8940605a2da0f643004ad16cc
lamoundioss7 <lamoundioss7@gmail.com>
2022-05-30 11:55:13 +0000

    forme de la page affiche users

---
+++ js/api.js
@@ -0,0 +1,16 @@
+
+console.log('On demare le projet consommation api')
+tab = []
+
+fetch('http://127.0.0.1:5000/api_groupe_7/users').then(function(res){ 
+    return res.json()
+}).then(function(data){ 
+    console.log(data.users);
+    return data.users
+
+})
+
+// console.log('hgjjghjggjgh',users)
+// users.then(function(resp) {
+//     console.log("ammmm",resp.json())
+// })
