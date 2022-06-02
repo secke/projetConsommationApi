e3f09c0ddc7e4aa9b8a15ee2a5046e83a7e92146
lamoundioss7 <lamoundioss7@gmail.com>
2022-06-02 14:43:38 +0000

    tepi taff taff yengall

--- js/api.js
+++ js/api.js
@@ -40,6 +40,23 @@ function createinput(div2,val){
     supreadonly(div4)
 }
 
+function data_form(line) {
+    inp[0].value= line.name
+    inp[1].value = line.username
+    inp[2].value = line.email
+    inp[3].value = line.address.street
+    inp[4].value = line.address.suite
+    inp[5].value = line.address.city
+    inp[6].value = line.address.zipcode
+    inp[7].value = line.address.geo.lat
+    inp[8].value = line.address.geo.long
+    inp[9].value = line.address.phone
+    inp[10].value = line.address.websit
+    inp[11].value = line.company.name
+    inp[12].value = line.company.catchPhrase
+    inp[13].value = line.company.bs
+}
+
 var a = 'name'
 function createbutton(div2,clas,text,line) {
     var div5 = div2.appendChild(document.createElement('button'))
@@ -49,72 +66,62 @@ function createbutton(div2,clas,text,line) {
         e.preventDefault()
         form.style.display = "block"
         elem.style.display = "none"
-        inp[0].value= line.name
-        console.log(inp)
-        inp[1].value = line.username
-        inp[2].value = line.email
-        inp[3].value = line.address.street
-        inp[4].value = line.address.suite
-        inp[5].value = line.address.city
-        inp[6].value = line.address.zipcode
-        inp[7].value = line.address.geo.lat
-        inp[8].value = line.address.geo.long
-        inp[9].value = line.address.phone
-        inp[10].value = line.address.websit
-        inp[11].value = line.company.name
-        inp[12].value = line.company.catchPhrase
-        inp[13].value = line.company.bs
-        j=0
-        // for(i in line){
-        //     inp[j].value=line[i]
-        //     j++
-        // }
-        
-        // for (elem of inp){
-        //     console.log(key)
-        // }
-        // console.log(form)
+        data_form(line)
         
         submit.addEventListener('click', (e)=>{
-            e.preventDefault()
-            fetch(`http://127.0.0.1:5000/api_groupe_7/users/${line.id}`, {
-
-                method : 'PUT',
-
-                headers: new Headers({"conten-Type" : "text/plain", "enctype":""}),
-
-                mode : 'cors',
-
-                cache : 'default',
+            e.preventDefault();
+            var id = String(line.id)
+            url = "http://127.0.0.1:5000/api_groupe_7/users/"+id
+            var resp = {
+                "name":inp[0].value,
+                "username":inp[1].value,
+                "email":inp[2].value,
                 
-                body :
-                    {
-                        "name":inp[0].value,
-                        "username":inp[1].value,
-                        "email":inp[2].value,
-                        "address":{
-                            "street":inp[3].value,
-                            "suite":inp[4].value,
-                            "city":inp[5].value,
-                            "zipcode":inp[6].value,
-                            "geo":{
-                                "lat":inp[7].value,
-                                "long":inp[8].value 
-                            },
-                            "phone":inp[9].value,
-                            "websit":inp[10].value
-                        },
-                        "company":{
-                            "name":inp[11].value,
-                            "catchPhrase":inp[12].value,
-                            "bs":inp[13].value
-                        }
-                    } 
-    
-                     
-            })
+                "street":inp[3].value,
+                "suite":inp[4].value,
+                "city":inp[5].value,
+                "zipcode":inp[6].value,
+                
+                "lat":inp[7].value,
+                "long":inp[8].value,
+                
+                "phone":inp[9].value,
+                "website":inp[10].value,
+                
+                
+                "companyName":inp[11].value,
+                "catchPhrase":inp[12].value,
+                "companyBs":inp[13].value
+                
+            } 
+            console.log(resp)
+            const option = {
+                method : 'PUT',
+                headers: {"Content-Type" : "application/json"},
+                mode: 'cors',
+                body : JSON.stringify(resp)
+            }
+            // class Put {
+            //     async put(url, resp) {
+            //         const reponse = await fetch(url, option)
+            //     }
+            // }
+            // var http = new Put;
+            // http.put(url,resp)
+
+            fetch(url, option)
+            .then(reponse=>reponse.json())
+            .then(data=>console.log(data))
+            .catch(err=>console.log(err))
+
+            form.style.display = "none"
+            elem.style.display = "block"
+
         })
 
+        
+        
+    
     })
     return div5
 }
@@ -146,21 +153,22 @@ function CreateElement( line ) {
 
 // let totalheight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-
+var btn_scroll = document.querySelector('.clickscrollbar')
 fetch('http://127.0.0.1:5000/api_groupe_7/users').then(function(res){ 
     return res.json()
 }).then(function(data){ 
-    console.log(data.users[0]);
-    for (var i = 0; i<=4; i++) {
+    // console.log(data.users[0]);
+    for (var i = 0; i<=6; i++) {
         CreateElement(data.users[i])
     }
     
     // document.body.scrollTop = 0
-    window.addEventListener('click', (e) => {
-        
+    btn_scroll.addEventListener('click', (e) => {
+        console.log(data.length)
         if (i>=10) {
             e.preventDefault()
             // i += 1
+            btn_scroll.style.display = "none"
         } else {
             CreateElement(data.users[i])
             
